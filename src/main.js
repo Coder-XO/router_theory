ab1b20fca415cddf25e710ebe65f6c4cf583bc02
Coder-XO <coderless@gmail.com>
2021-04-28 16:02:52 +0800

    history模式的实现

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ const app = document.querySelector("#app")   // 初始容器
 //第一层路由
 const div1 = document.createElement('div')
 div1.innerHTML = '1'
-const view1 = document.createElement('div')
-div1.appendChild(view1)
-view1.style.height = '50px'
-view1.style.background = 'red'
 const div2 = document.createElement('div')
 div2.innerHTML = '2'
 const div3 = document.createElement('div')
@@ -13,29 +9,16 @@ div3.innerHTML = '3'
 const div4 = document.createElement('div')
 div4.innerHTML = '4'
 const routeTable = {    // 路由表
-    '1': div1,
-    '2': div2,
-    '3': div3,
-    '4': div4,
-}
-// 嵌套的路由
-const div11 = document.createElement('div')
-div1.innerHTML = '1.1'
-const div12 = document.createElement('div')
-div1.innerHTML = '1.2'
-const div13 = document.createElement('div')
-div1.innerHTML = '1.3'
-const div14 = document.createElement('div')
-div1.innerHTML = '1.4'
-const route1Table = {
-    '1/1':div11,
-    '1/2':div12,
-    '1/3':div13,
-    '1/4':div14,
+    '/1': div1,
+    '/2': div2,
+    '/3': div3,
+    '/4': div4,
 }
 
 function route(container) {    // 参数代表显示页面的容器
-    let number = window.location.hash.substr(1)
+    // let number = window.location.hash.substr(1)
+    let number = window.location.pathname
+    console.log(number)
     let app = document.querySelector('#app')
     number = number || 1    // 默认路由
     // 获取界面
@@ -49,9 +32,21 @@ function route(container) {    // 参数代表显示页面的容器
     container.appendChild(div)
 }
 
+const allA = document.querySelectorAll('.link')  //获取页面a标签
+for (let taga of allA) {
+    taga.addEventListener('click', (e) => {
+        e.preventDefault()
+        const href = taga.getAttribute('href')
+        window.history.pushState(null, 'page 2', href)
+        //  通知
+        onStateChange(href)
+    })
+}
+
 route(app)
 
-//   用户改变URL中hash事件
-window.addEventListener('hashchange', () => {
+function onStateChange() {
+    console.log('state变了')
     route(app)
-})
+}
+
